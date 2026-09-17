407a31f0af86bf4ab8f982583528bb63a5c73c18
EliM1706 <elizabeth.minaba@campusucc.edu.co>
2026-09-17 01:33:52 +0000

    NUEVAS COSAS

--- _pycache_/main.py
+++ _pycache_/main.py
@@ -1,31 +1,8 @@
-import json
-from fastapi import FastAPI
-
-app= FastAPI()
-
-@app.get("/")
-def inicio():
-    return {
-        "mensaje": "Generaciòn de la API"
-    }
-
-@app.get("/clientes")
-def obtener_clientes():
-    with open("data/clientes.json", "r") as archivo:
-        clientes = json.load(archivo)
-
-    return clientes   
-
-
-@app.get("/clientes/{id_cliente}")
-def obtener_cliente(id_cliente: int): 
-    with open("data/clientes.json", "r") as archivo:
-            clientes = json.load(archivo)
-
-    for cliente in clientes:
-        if cliente["id"] == id_cliente:
-             return cliente
-
-    return{
-         "error": "cliente no encontrado"
-    }
+1. Elizabeth: Carga, Validación de Estructura y Limpieza Base
+Su enfoque está en la fase de Extracción y la primera parte de Transformación.
+ Tareas asignadas:
+1. Portada e Importación: Crear la portada en Markdown e importar las librerías necesarias (⁠pandas⁠, ⁠numpy⁠, etc.) explicando la función de cada una.
+2. Carga de Datos: Importar el archivo original ⁠hour.csv⁠ sin modificaciones previas en Excel y documentar dimensiones iniciales (filas y columnas).
+3. Inspección Inicial: Mostrar los tipos de datos, primeros registros y crear la tabla/diccionario con la descripción de las variables.
+4. Gestión Temporal Base: Convertir la columna ⁠dteday⁠ a formato datetime y crear el identificador temporal ⁠DateTime⁠ uniendo fecha y hora (⁠hr⁠).
+5. Verificación de Huecos/Duplicados: Ordenar la serie cronológicamente y comprobar que no existan duplicados ni nulos en el conjunto original.
