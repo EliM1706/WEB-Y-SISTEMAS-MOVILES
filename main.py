09d3c3f8e55ecfffb019a6ecbb20bc8f422d1e25
EliM1706 <elizabeth.minaba@campusucc.edu.co>
2026-09-17 00:42:28 +0000

    dos carpetas

---
+++ main.py
@@ -0,0 +1,31 @@
+import json
+from fastapi import FastAPI
+
+app= FastAPI()
+
+@app.get("/")
+def inicio():
+    return {
+        "mensaje": "Generaciòn de la API"
+    }
+
+@app.get("/clientes")
+def obtener_clientes():
+    with open("data/clientes.json", "r") as archivo:
+        clientes = json.load(archivo)
+
+    return clientes   
+
+
+@app.get("/clientes/{id_cliente}")
+def obtener_cliente(id_cliente: int): 
+    with open("data/clientes.json", "r") as archivo:
+            clientes = json.load(archivo)
+
+    for cliente in clientes:
+        if cliente["id"] == id_cliente:
+             return cliente
+
+    return{
+         "error": "cliente no encontrado"
+    }
